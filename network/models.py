8f68f8338e9fd6594bbb16cab51e8d56c41eb1e6
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2023-10-17 14:34:05 -0500

    Add Post, Like and Follow models

--- network/models.py
+++ network/models.py
@@ -8,3 +8,21 @@ class User(AbstractUser):
     id = models.AutoField(primary_key=True)
     pass
 
+
+# Post
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    content = models.CharField(max_length=400)
+    date = models.DateTimeField(default=timezone.now)
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# Like
+class Like(models.Model):
+    id = models.AutoField(primary_key=True)
+    liker = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# Follower
+class Follow(models.Model):
+    id = models.AutoField(primary_key=True)
+    follower = models.ForeignKey(User, on_delete=models.CASCADE)
+    followed = models.ForeignKey(User, on_delete=models.CASCADE)
