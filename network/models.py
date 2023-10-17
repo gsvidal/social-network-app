12fab858e981e97e5b848d0d346ac30d90533df6
Gonzalo Vidal <gonzalovidal.dev@gmail.com>
2023-10-17 14:33:13 -0500

    First commit

---
+++ network/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+
+
+
+class User(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    pass
+
